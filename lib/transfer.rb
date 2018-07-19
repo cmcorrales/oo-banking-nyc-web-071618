c20d609aee2e21e3b8011a0a0d75cc8dfc5a5149
cmcorrales <cmcorrales3@gmail.com>
2018-07-19 16:04:13 -0400

    complete transfer and bank_account classes

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,40 @@
+require "pry"
+
 class Transfer
+  attr_accessor :sender, :receiver, :status, :amount
   # your code here
+  def initialize(sender, receiver, amount)
+    #puts "print agrs :",name.name, sender.name, arg3
+    #binding.pry
+    @receiver = receiver
+    @sender=sender 
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    @sender.valid? && @receiver.valid?
+  end
+
+  def execute_transaction()
+    # binding.pry
+    if valid? == true && @sender.balance > @amount && @status == "pending"
+        @sender.balance -= @amount
+        @receiver.balance += @amount
+        @status = "complete" 
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      @receiver.balance -= @amount
+      @sender.balance += @amount
+      @status = "reversed"
+    end
+  end
+
+
 end
